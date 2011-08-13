c5e3527923bd7bd96002b042b661de7b1b89da85
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2011-08-13 08:17:11 +0530

    updated .apk in /bin

--- gen/com/apress/proandroidmedia/ch12/blipuploader/R.java
+++ gen/com/apress/proandroidmedia/ch12/blipuploader/R.java
@@ -14,6 +14,8 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int captureVideo=0x7f050002;
+        public static final int selectVideo=0x7f050001;
         public static final int textview=0x7f050000;
     }
     public static final class layout {
